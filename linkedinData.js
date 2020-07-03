09ba0787ef1f22c7d8f749dc05e1b0b27e11f38b
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-07-03 22:01:42 +0530

    NavBar design done and json file is created manually

---
+++ linkedinData.js
@@ -0,0 +1,56 @@
+const linkedinData = [
+  {
+    id: 1,
+    user: "Tezi khan",
+    description: "Software Engineer",
+    content:
+      "Which CSS framework is good for Angular Projects\napart from bootstrap",
+    likes: 50,
+    comments: 10,
+  },
+  {
+    id: 2,
+    user: "Harshit Ahluwalla",
+    description: "Data Scientist",
+    content:
+      "Recent #datascience #internship and #jobs\nComment 'Add me' and I will add you to our close Telegram channel\nData Science Internship - Corner Stone Solutions\nData Science Internship - UpTricks Services\nPlease find all above internship here: https://bit.ly/2YAhEQu\n---- Hope this helps!\n#dataanalyst #jobs #deeplearning #machinelearning #harshitahluwalia",
+    likes: 200,
+    comments: 10,
+  },
+  {
+    id: 3,
+    user: "Arpit Shrotriya",
+    description: "App Developer Intern",
+    content:
+      "Hey Folks!\nHope everyone is doing well.\nAs #SIH2020 is coming closer than ever! And Gearing up to be an online event this year (that's a bummer tbh).\nIt would be really helpful for us if any of you great developers would like to help us in the field of Web Development for our project and become the mentor of Team Kaizen for #SIH2020.\nTeam Kaizen: Hemshree Madaan Shubham Gupta SUMIT KUMAR Simrann Arora Anirudh Jain\nHope A mentoring opportunity interest you.\n#SIH #Mentor #Developers #Project",
+    likes: 30,
+    comments: 10,
+  },
+  {
+    id: 4,
+    user: "Tezi khan",
+    description: "Software Engineer",
+    content:
+      "Which CSS framework is good for Angular Projects\napart from bootstrap",
+    likes: 50,
+    comments: 10,
+  },
+  {
+    id: 5,
+    user: "Harshit Ahluwalla",
+    description: "Data Scientist",
+    content:
+      "Recent #datascience #internship and #jobs\nComment 'Add me' and I will add you to our close Telegram channel\nData Science Internship - Corner Stone Solutions\nData Science Internship - UpTricks Services\nPlease find all above internship here: https://bit.ly/2YAhEQu\n---- Hope this helps!\n#dataanalyst #jobs #deeplearning #machinelearning #harshitahluwalia",
+    likes: 200,
+    comments: 10,
+  },
+  {
+    id: 6,
+    user: "Arpit Shrotriya",
+    description: "App Developer Intern",
+    content:
+      "Hey Folks!\nHope everyone is doing well.\nAs #SIH2020 is coming closer than ever! And Gearing up to be an online event this year (that's a bummer tbh).\nIt would be really helpful for us if any of you great developers would like to help us in the field of Web Development for our project and become the mentor of Team Kaizen for #SIH2020.\nTeam Kaizen: Hemshree Madaan Shubham Gupta SUMIT KUMAR Simrann Arora Anirudh Jain\nHope A mentoring opportunity interest you.\n#SIH #Mentor #Developers #Project",
+    likes: 30,
+    comments: 10,
+  },
+];
